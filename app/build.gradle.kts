ff2880c9db4155adcbec183d469a688d04eb2d83
polaraju1005 <yalagantisaipolaraju@gmail.com>
2023-12-19 11:44:57 +0530

    Splash Screen Frontend Completed

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.justicechain"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.justicechain"
